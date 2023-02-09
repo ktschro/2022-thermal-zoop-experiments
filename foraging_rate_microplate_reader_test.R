dd64c428396ec8ad4b65ce04c48bf5a1810889df
Katie Schroeder <kschro011@gmail.com>
2023-02-09 14:59:21 -0500

    microplate reader trial data

---
+++ foraging_rate_microplate_reader_test.R
@@ -0,0 +1,25 @@
+plate<-read.csv("Schroeder_Strauss_2_3_2023_microplate reader_trial_2_all_data.csv")
+plate$plate<-as.factor(plate$plate)
+
+library(tidyverse)
+library(ggpubr)
+library(ggplot2)
+
+plate %>% ggerrorplot(x="conc_ank",y="reading_485_665",desc_stat="mean_sd",facet.by = "plate")
+
+plate %>% ggplot(aes(x=conc_ank,y=reading_485_665,group=conc_ank)) + theme_classic() + geom_boxplot() +facet_wrap(.~plate)
+
+plate_2 <- plate %>% filter(plate=="2")
+
+m1<-lm(reading_485_665~conc_ank,data=plate_2)
+
+#check assumptions - some funneling, fine
+plot(m1)
+hist(m1$residuals)
+
+summary(m1)
+
+#add predicted values
+plate_2$pred_fluor <- fitted(m1)
+
+ggplot(data=plate_2,aes(x=conc_ank,y=reading_485_665)) +geom_point()+geom_line(aes(x=conc_ank,y=pred_fluor))+theme_classic()
